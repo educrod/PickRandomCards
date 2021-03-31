527ac4b57484cbda368778996b8dcbe361aaff95
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-03-31 15:51:07 -0300

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RadomValue() + " of " + RamsomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RamsomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            return "Diamonds";
+        }
+
+        private static string RadomValue()
+        {
+            // get random number from 1 to 14
+            int value = random.Next(1, 14);
+            // if it's 1 return the string Ace
+            if (value == 1) return "Ace";
+            // if it's 1 return the string Jack
+            if (value == 11) return "Jack";
+            // if it's 1 return the string Queen
+            if (value == 12) return "Queen";
+            // if it's 1 return the string King
+            if (value == 13) return "King";
+            // if we haven't returned yet, return the number and convert it to string
+            return value.ToString();
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                // this block is executed if line COULD be converted to an int
+                // value that's stored in a new variable called numberOfCards
+
+                string[] cards = new string[numberOfCards];
+                cards = CardPicker.PickSomeCards(numberOfCards);
+                foreach (string card in cards)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                // this block is executes if line COULD NOT ne converted to an int
+                Console.WriteLine("Check if it's really a number");
+            }
+        }
+    }
+}
